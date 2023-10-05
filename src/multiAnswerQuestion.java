e9fbc0218f29907cd60f9fe9304f4602b33a1255
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2023-10-05 13:15:34 -0700

    Update multiAnswerQuestion.java

--- src/multiAnswerQuestion.java
+++ src/multiAnswerQuestion.java
@@ -1,13 +1,18 @@
 import java.util.List;
+import java.util.Set;
 
 public class multiAnswerQuestion implements QuestionInterface {
     private final List<String> choices;
+    private final int[] correctAnswers;
 
-    public multiAnswerQuestion(List<String> choices) {
+    public multiAnswerQuestion(List<String> choices, int[] correctAnswers) {
         this.choices = choices;
+        this.correctAnswers = correctAnswers;
     }
 
     public List<String> getChoices() {
         return choices;
     }
+
+    public int[] getCorrectAnswers() { return correctAnswers; }
 }
