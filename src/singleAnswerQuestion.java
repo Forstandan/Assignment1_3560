1a23a7dd012e595fbae8d0f7b697ea390655c381
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2023-10-05 13:17:09 -0700

    Update singleAnswerQuestion.java

--- src/singleAnswerQuestion.java
+++ src/singleAnswerQuestion.java
@@ -2,12 +2,18 @@ import java.util.List;
 
 public class singleAnswerQuestion implements QuestionInterface {
     private final List<String> choices;
+    private final int[] correctAnswers;
 
-    public singleAnswerQuestion(List<String> choices) {
+    public singleAnswerQuestion(List<String> choices, int[] correctAnswers) {
         this.choices = choices;
+        this.correctAnswers = correctAnswers;
     }
 
     public List<String> getChoices() {
         return choices;
+    }
+
+    public int[] getCorrectAnswers() {
+        return correctAnswers;
     }
 }
